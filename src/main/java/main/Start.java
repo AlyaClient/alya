beaf6f5af5861357721cea8fb31a4bcb3e958fc1
thoq <tristanshaw000@gmail.com>
2026-03-14 21:58:07 -0400

    what

---
+++ src/main/java/main/Start.java
@@ -0,0 +1,23 @@
+package main;
+import dev.thoq.util.misc.Array;
+import dev.thoq.util.misc.Platform;
+import net.minecraft.client.main.Main;
+
+public class Start {
+
+    public static void main(String[] args) {
+        Platform.apply();
+        Main.main(
+            Array.concat(
+                new String[]{
+                        "--version", "1.8.9",
+                        "--accessToken", "0",
+                        "--assetsDir", "assets",
+                        "--userProperties", "{}"
+                }, args
+            )
+        );
+    }
+
+
+}
