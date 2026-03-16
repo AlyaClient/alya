06a81e7d1e3f3fe61933acc98020316040b708dc
Thoq <tristanshaw000@gmail.com>
2026-03-15 20:09:52 -0400

    buh???

---
+++ dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+JDK_PATH="$HOME/.sdkman/candidates/java/8.0.482-tem/bin/java"
+SCRIPT_DIR=$(pwd)
+
+CP="$SCRIPT_DIR/target/classes:$SCRIPT_DIR/jars/versions/1.8.9/1.8.9.jar:$SCRIPT_DIR/target/libs"
+
+./mvnw compile
+
+$JDK_PATH "-XX:HeapDumpPath=$SCRIPT_DIR/Alya.dev.heapdump" \
+  "-Djava.library.path=$SCRIPT_DIR/jars/versions/1.8.9/1.8.9-natives" \
+  "-Dminecraft.launcher.brand=minecraft-launcher" \
+  "-Dminecraft.launcher.version=3.2.13" \
+  -cp "$CP" \
+  -Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC \
+  -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M \
+  "-Dlog4j.configurationFile=$SCRIPT_DIR/assets/log_configs/client-1.7.xml" \
+  main.Start --gameDir "$SCRIPT_DIR/jars/.minecraft" \
+  --assetIndex 1.8 --uuid 887341c45ea94dd3bae9bb00502124f1 --userType msa
