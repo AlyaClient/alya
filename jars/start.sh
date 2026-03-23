4c829501127cac87b48ed18d2b3a69cdbd512a32
Thoq <tristanshaw000@gmail.com>
2026-03-22 22:25:19 -0400

    add user start script

--- jars/start.sh
+++ jars/start.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
+# Hey you!
+# This script is intended for end-users who do not want to
+# use a GUI launcher. Do not run if developing.
+
 JDK_PATH="$HOME/.sdkman/candidates/java/21.0.10-zulu/bin/java"
 echo "JDK PATH: $JDK_PATH"
 
 SCRIPT_DIR=$(pwd)
-
-CP="$SCRIPT_DIR/target/Alya-1.0.jar:$SCRIPT_DIR/target/libs"
-
-cd "$SCRIPT_DIR/jars"
+CP="$SCRIPT_DIR/Alya.jar:$SCRIPT_DIR/libs"
 
 $JDK_PATH "-XX:HeapDumpPath=$SCRIPT_DIR/Alya.heapdump" \
-  "-Djava.library.path=$SCRIPT_DIR/jars/versions/1.8.9/1.8.9-natives" \
+  "-Djava.library.path=$SCRIPT_DIR/natives" \
   "-Dminecraft.launcher.brand=minecraft-launcher" \
   "-Dminecraft.launcher.version=3.2.13" \
   "-Dio.netty.transport.noNative=true" \
-  "-Dminecraft.client.jar=$SCRIPT_DIR/target/Alya-1.0.jar" \
+  "-Dminecraft.client.jar=$SCRIPT_DIR/Alya.jar" \
   -cp "$CP" \
   -Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC \
   -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M \
-  "-Dlog4j.configurationFile=$SCRIPT_DIR/jars/.minecraft/assets/log_configs/client-1.7.xml" \
-  start.Main --gameDir "$SCRIPT_DIR/jars/.minecraft" \
+  "-Dlog4j.configurationFile=$SCRIPT_DIR/.minecraft/assets/log_configs/client-1.7.xml" \
+  start.Main --gameDir "$SCRIPT_DIR/.minecraft" \
   --assetIndex 1.8 --uuid 0 --userType msa
