976f9d3c43b1805be44226ca1bf6247a9a60092a
thoq <tristanshaw000@gmail.com>
2026-04-10 09:28:26 -0400

    update run script to suppress warnings

--- jars/start.sh
+++ jars/start.sh
@@ -25,6 +25,8 @@ $JDK_PATH "-XX:HeapDumpPath=$SCRIPT_DIR/Alya.heapdump" \
   "-Dminecraft.launcher.brand=minecraft-launcher" \
   "-Dminecraft.launcher.version=3.2.13" \
   "-Dio.netty.transport.noNative=true" \
+  --enable-native-access=ALL-UNNAMED \
+  --sun-misc-unsafe-memory-access=allow \
   "-Dminecraft.client.jar=$SCRIPT_DIR/Alya.jar" \
   $EXTRA_FLAGS -cp "$CP" -Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC \
   -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M \
